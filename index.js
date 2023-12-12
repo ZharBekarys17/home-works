18fdfc976a02e1cb54e1d0b68bedb33a4a2d54b1
ZharBekarys17 <149668236+ZharBekarys17@users.noreply.github.com>
2023-12-12 14:29:03 +0600

    javascript
    Bekarys

---
+++ index.js
@@ -0,0 +1,58 @@
+console.log("Hello world") //print()
+console.log(5)
+console.log(true)
+let age = 32 // айнымалы береміз
+let name = 'Bekarys'
+console.log(age)
+let city = 'Almaty'
+let country = `Kazakhstan`
+console.log(city, country, age)
+console.log(`My name is ${name} I'm from ${country} and I'm a ${age} years old`)
+
+let first_number = 5
+let second_number = 6
+console.log(`${first_number}*${second_number}=${first_number * second_number}`)
+
+let a = 4536
+let b = 237
+let c = 485
+let d = 123
+let total = a*b+c*d
+console.log(`${a}*${b}+${c}*${d}=${total}`)
+// const name1 = 'Erik'
+// name1 = 'Kripke'
+// 1. Declare variables favoriteColor, favoriteSong, favoriteMovie, favoriteAnimal, and so one and assign them to things you 
+// like. Make a story (рассказ мин. 50-60 слов) titled “What I love” using this variables in Kazakh.
+const favoriteColor = 'green'
+const favourite_song = 'My dream sweat baby!????'
+const favoriteMovie = 'Hunter Freelanser'
+const favoriteAnimal = 'Penguins'
+const favoriteCountry = 'Southern Hemisphere'
+const favouriteFood = 'tikka masala'
+const favoriteHobby = 'be confuse'
+const favoritePlanet = 'Xylopticon-9'
+const favoriteCharacter = 'Zorandria'
+const favouriteEvil = 'Korvex'
+const myPat = 'Zylthor'
+const myfriendNickname = '李伟'
+const friendhome = '新长'
+console.log(`Сәлем менің атым ${name}.Mен ${age} жастамын, менің сүйікті тамағым ол ${favouriteFood} әсіресе тауық пен. Себебі мен тауықты жақсы көрем. Кәзіргі жағдай да мен де досым жоқ , бірақ мен оны былпһай ойдан құрай аламын, оның аты ${myfriendNickname} және ол қытайдан келген. Біз ${myfriendNickname} екуміз ғаламшар туралы мультиктерді қарауды жақсы көреміз оның сүйікті кейіпкері ол ${favoriteCharacter} меніңде , сонымен қатар екуміз бірдей кино ны  жақсы көреміз ол ${favoriteMovie} және де бір жануар ${myPat} `)
+
+// 1. Declare a variable hasCoffee and set it to true. Log a message indicating whether you have coffee or not.
+let hasCoffee = true
+
+console.log (`Бүгін таңертенде менде кофе ${hasCoffee} болды`)
+// 2. Declare a variable isWeekend and set it to false. Log a message indicating whether it's the weekend or not.
+let isWeekend = false
+console.log(`today is monday, so it is not ${isWeekend}`)
+//Float Tasks:
+// 1. Declare a variable gravity and assign it the gravitational acceleration on Earth (9.8 m/s²). Calculate and print a force of gravity of a person weighing 60 kg.
+const gravity = 9.8
+const person_weight = 60
+
+let gravity_total = gravity * person_weight
+console.log(`the result is ${gravity_total}`)
+//2. Declare a variable temp and assign it a floating-point value representing the current temperature. Convert it to Fahrenheit and log it to console.
+const cel = 25.0
+const total_fahrenheit = (cel * 9/5) + 32
+console.log(`the total fahrenheiet temp is - ${total_fahrenheit}`)
